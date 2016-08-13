803bd10e74c555f440c3065500b577b309a63cdc
BinaryMoon <binarymoon@gmail.com>
2016-08-13 19:15:22 +0100

    update html

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html class="no-js" lang="en">
     <head>
+
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="profile" href="http://gmpg.org/xfn/11">
@@ -10,9 +11,10 @@
 
         <title>Master WordPress</title>
 
-        <link rel="stylesheet" href="" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,300italic" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,900&subset=latin-ext" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css" />
+        <link rel="stylesheet" href="assets/css/styles.css" />
+
     </head>
 
     <body>
@@ -27,62 +29,77 @@
 
         <section class="intro">
 
-            A free seven-day email course to
+            <div class="content">
 
-            <h2>Become a WordPress Master</h2>
+                A free seven-day email course to
 
-            <p>Learn everything you need to master WordPress, not rely on developers, customize your WordPress theme, integrate marketing, secure your site, and more.</p>
+                <h2>Become a WordPress Master</h2>
 
-            <p>Your favourite email address ~ [Get lesson one]</p>
+                <p>Learn everything you need to master WordPress, not rely on developers, customize your WordPress theme, integrate marketing, secure your site, and more.</p>
+
+                <p>Your favourite email address ~ [Get lesson one]</p>
 
 
-            <p>MasterWP is a completely free seven-day email course. We’ll never share your personal information with third parties.</p>
+                <p>MasterWP is a completely free seven-day email course. We’ll never share your personal information with third parties.</p>
 
 
-            <p>Most WordPress training courses focus on the basics: how to add pages, how to install themes. If you want to do anything more complicated they say hire a developer.</p>
+                <p>Most WordPress training courses focus on the basics: how to add pages, how to install themes. If you want to do anything more complicated they say hire a developer.</p>
 
-            <p>This is fine, but how about bridging the gap between user and developer?</p>
+                <p>This is fine, but how about bridging the gap between user and developer?</p>
 
-            <p>We’ve made a course that teaches WordPress users how to Master WordPress.</p>
+                <p>We’ve made a course that teaches WordPress users how to Master WordPress.</p>
+
+            </div>
 
         </section>
 
         <section class="course-lessons">
 
-            <p>In this free course you’ll leanr the skills you need to do everything you want with WordPress yourself, without the need for paid
-            help. The seven lessons will cover:</p>
+            <div class="content">
 
-            <ul>
-                <li><strong>Lesson 1:</strong> how to choose the perfect WordPress theme</li>
-                <li><strong>Lesson 2:</strong> get the most from theme support</li>
-                <li><strong>Lesson 3:</strong> theme customization basics (+ helpful plugins)</li>
-                <li><strong>Lesson 4:</strong> make your site load really quickly</li>
-                <li><strong>Lesson 5:</strong> using CDNs for extra speed</li>
-                <li><strong>Lesson 6:</strong> integrating email marketing with WordPress</li>
-                <li><strong>Lesson 7:</strong> keeping your WordPress site secure</li>
-            </ul>
+                <p>In this free course you’ll learn the skills you need to do everything you want with WordPress yourself, without the need for paid help. The seven lessons will cover:</p>
+
+                <ul>
+                    <li><strong>Lesson 1:</strong> how to choose the perfect WordPress theme</li>
+                    <li><strong>Lesson 2:</strong> get the most from theme support</li>
+                    <li><strong>Lesson 3:</strong> theme customization basics (+ helpful plugins)</li>
+                    <li><strong>Lesson 4:</strong> make your site load really quickly</li>
+                    <li><strong>Lesson 5:</strong> using CDNs for extra speed</li>
+                    <li><strong>Lesson 6:</strong> integrating email marketing with WordPress</li>
+                    <li><strong>Lesson 7:</strong> keeping your WordPress site secure</li>
+                </ul>
+
+            </div>
 
         </section>
 
         <section class="about-us">
 
-            <h2>About the authors</h2>
+            <div class="content">
 
-            <p>Hey, I’m Ben! I’m a WordPress theme developer. I built one of the most popular themes on WordPress.com, one of the first “magazine style” themes and co-founded leading WordPress theme shop ProThemeDesign.</p>
+                <h2>About the authors</h2>
 
-            <p>I’m an experienced developer, serving for over a decade as Director of Web Development at one of the largest websites in the world. I’ll be leading the technical lessons to equip you with the skills you need to Master WordPress.</p>
+                <p>Hey, I’m Ben! I’m a WordPress theme developer. I built one of the most popular themes on WordPress.com, one of the first “magazine style” themes and co-founded leading WordPress theme shop <a href="https://prothemedesign.com/">Pro Theme Design</a>.</p>
 
-            <p>Hey, I’m Alex! I’m a WordPress power-user. I founded one of the most popular WordPress tutorial blogs and have written hundreds of WordPress tutorials for Smashing Magazine, WPShout, WPZOOM and more.</p>
+                <p>I’m an experienced developer, serving for over a decade as Director of Web Development at one of the largest websites in the world. I’ll be leading the technical lessons to equip you with the skills you need to Master WordPress.</p>
 
-            <p>I’ve written support, documentation and video tutorials for WordPress companies and understand what users need to know. I’ll be leading the knowhow-based lessons so you can handle everything you need with WordPress yourself.</p>
+                <p>Hey, I’m Alex! I’m a WordPress power-user. I founded one of the most popular WordPress tutorial blogs and have written hundreds of WordPress tutorials for Smashing Magazine, WPShout, WPZOOM and more.</p>
+
+                <p>I’ve written support, documentation and video tutorials for WordPress companies and understand what users need to know. I’ll be leading the knowhow-based lessons so you can handle everything you need with WordPress yourself.</p>
+
+            </div>
 
         </section>
 
         <footer>
 
-            <p>Still unsure about signing up? Become a WordPress Master is 100% free, there’s literally nothing to lose and you can unsubscribe at any time. Sign up and start the course immediately.</p>
+            <div class="content">
 
-            Your favourite email address ~ [Get lesson one]
+                <p>Still unsure about signing up? Become a WordPress Master is 100% free, there’s literally nothing to lose and you can unsubscribe at any time. Sign up and start the course immediately.</p>
+
+                Your favourite email address ~ [Get lesson one]
+
+            </div>
 
         </footer>
 
