bfccfe9d7efe84df78943406a771bf8f9156f936
BinaryMoon <binarymoon@gmail.com>
2016-08-25 11:29:21 +0100

    add name field to newsletter form

--- partials/newsletter.php
+++ partials/newsletter.php
@@ -1,5 +1,10 @@
 <form class="newsletter">
+
+    <label for="name">Name:</label>
+    <input type="text" name="name" id="name" placeholder="Jo" />
+
     <label for="email">Email Address:</label>
     <input type="text" name="email" id="email" placeholder="jo@masterwp.co" />
+
     <input type="submit" value="Start Lesson 1" />
 </form>
