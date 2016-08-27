e197520e90bc2169d324478d8f004c9fa461759f
BinaryMoon <binarymoon@gmail.com>
2016-08-27 16:56:25 +0100

    add Google analytics and hide header

--- index.php
+++ index.php
@@ -14,16 +14,29 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Catamaran:300,700|Playfair+Display:400,700" />
         <link rel="stylesheet" href="assets/css/styles.css" />
 
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-55722-24', 'auto');
+          ga('send', 'pageview');
+
+        </script>
+
     </head>
 
     <body>
 
         <header class="masthead">
 
+            <!--
             <div class="branding">
                 <img src="assets/images/logo-mark.png" alt="MasterWP Logo" />
                 <h1>Master<span>WP</span></h1>
             </div>
+            -->
 
 
             <div class="container">
