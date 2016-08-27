e5f2205881866e00054607aea1af0d6202793c4a
BinaryMoon <binarymoon@gmail.com>
2016-08-27 09:09:31 +0100

    tweak newsletter signup styles

--- partials/newsletter.php
+++ partials/newsletter.php
@@ -3,8 +3,8 @@
     <label for="name">Name:</label>
     <input type="text" name="name" id="name" placeholder="Jo" />
 
-    <label for="email">Email Address:</label>
-    <input type="text" name="email" id="email" placeholder="jo@masterwp.co" />
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email" placeholder="jo@masterwp.co" />
 
     <input type="submit" value="Start Lesson 1" />
 </form>
