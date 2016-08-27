a47be4b7b58e0a436f86d6c67e36f6da76c04cda
BinaryMoon <binarymoon@gmail.com>
2016-08-27 12:57:44 +0100

    add author profile photos

--- index.php
+++ index.php
@@ -111,7 +111,9 @@
 
                 <div class="person">
 
-                    <p>Hey, I’m Ben! I’m a WordPress theme developer. I built one of the most popular themes on WordPress.com, one of the first “magazine style” themes and co-founded leading WordPress theme shop <a href="https://prothemedesign.com/">Pro Theme Design</a>.</p>
+                    <img src="assets/images/profile-ben.jpg" alt="Ben Gillbanks - WordPress developer" />
+
+                    <p><strong>Hi, I’m Ben!</strong> I’m a WordPress theme developer. I built one of the most popular themes on WordPress.com, one of the first “magazine style” themes and co-founded leading WordPress theme shop <a href="https://prothemedesign.com/">Pro Theme Design</a>.</p>
 
                     <p>I’m an experienced developer, serving for over a decade as Director of Web Development at one of the largest websites in the world. I’ll be leading the technical lessons to equip you with the skills you need to Master WordPress.</p>
 
@@ -119,7 +121,9 @@
 
                 <div class="person">
 
-                    <p>Hey, I’m Alex! I’m a WordPress power-user. I founded one of the most popular WordPress tutorial blogs and have written hundreds of WordPress tutorials for Smashing Magazine, WPShout, WPZOOM and more.</p>
+                    <img src="assets/images/profile-alex.jpg" alt="Alex Denning - WordPress power-user" />
+
+                    <p><strong>Hey, I’m Alex!</strong> I’m a WordPress power-user. I founded one of the most popular WordPress tutorial blogs and have written hundreds of WordPress tutorials for Smashing Magazine, WPShout, WPZOOM and more.</p>
 
                     <p>I’ve written support, documentation and video tutorials for WordPress companies and understand what users need to know. I’ll be leading the knowhow-based lessons so you can handle everything you need with WordPress yourself.</p>
 
