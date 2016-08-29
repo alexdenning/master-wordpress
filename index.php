9fba0183e601777c721f9f794fe35e6b44954c4f
BinaryMoon <binarymoon@gmail.com>
2016-08-29 08:00:50 +0100

    change position of 'about us' section

--- index.php
+++ index.php
@@ -38,7 +38,6 @@
             </div>
             -->
 
-
             <div class="container">
 
                 <div class="banner">
@@ -85,6 +84,36 @@
 
         <hr />
 
+        <section class="about-us">
+
+            <h2>About the Authors</h2>
+
+            <div class="people">
+
+                <div class="person">
+
+                    <img src="assets/images/profile-ben.jpg" alt="Ben Gillbanks - WordPress developer" />
+
+                    <p><strong>Hi, I’m Ben!</strong> I’m a WordPress theme developer. I built one of the most popular themes on WordPress.com, one of the first “magazine style” themes and co-founded leading WordPress theme shop <a href="https://prothemedesign.com/">Pro Theme Design</a>.</p>
+
+                    <p>I’m an experienced developer, serving for over a decade as Director of Web Development at one of the largest websites in the world. I’ll be leading the technical lessons to equip you with the skills you need to Master WordPress.</p>
+
+                </div>
+
+                <div class="person">
+
+                    <img src="assets/images/profile-alex.jpg" alt="Alex Denning - WordPress power-user" />
+
+                    <p><strong>Hey, I’m Alex!</strong> I’m a WordPress power-user. I founded one of the most popular WordPress tutorial blogs and have written for Smashing Magazine, WPShout, WPZOOM and more.</p>
+
+                    <p>I’ve created documentation and video tutorials for WordPress companies and understand what users need to know. I’ll be leading the knowhow-based lessons so you can handle everything you need with WordPress yourself.</p>
+
+                </div>
+
+            </div>
+
+        </section>
+
         <section class="course-lessons">
 
             <div class="content">
@@ -121,38 +150,6 @@
                         <strong>Lesson 7:</strong> Keeping your WordPress site secure
                     </li>
                 </ul>
-
-            </div>
-
-        </section>
-
-        <hr />
-
-        <section class="about-us">
-
-            <h2>About the Authors</h2>
-
-            <div class="people">
-
-                <div class="person">
-
-                    <img src="assets/images/profile-ben.jpg" alt="Ben Gillbanks - WordPress developer" />
-
-                    <p><strong>Hi, I’m Ben!</strong> I’m a WordPress theme developer. I built one of the most popular themes on WordPress.com, one of the first “magazine style” themes and co-founded leading WordPress theme shop <a href="https://prothemedesign.com/">Pro Theme Design</a>.</p>
-
-                    <p>I’m an experienced developer, serving for over a decade as Director of Web Development at one of the largest websites in the world. I’ll be leading the technical lessons to equip you with the skills you need to Master WordPress.</p>
-
-                </div>
-
-                <div class="person">
-
-                    <img src="assets/images/profile-alex.jpg" alt="Alex Denning - WordPress power-user" />
-
-                    <p><strong>Hey, I’m Alex!</strong> I’m a WordPress power-user. I founded one of the most popular WordPress tutorial blogs and have written for Smashing Magazine, WPShout, WPZOOM and more.</p>
-
-                    <p>I’ve created documentation and video tutorials for WordPress companies and understand what users need to know. I’ll be leading the knowhow-based lessons so you can handle everything you need with WordPress yourself.</p>
-
-                </div>
 
             </div>
 
