84cb7c28fc5a8fe0819434dd9e30d973ab4f4eca
BinaryMoon <binarymoon@gmail.com>
2016-08-29 20:33:03 +0100

    hook the signup form to mailchimp

--- partials/newsletter.php
+++ partials/newsletter.php
@@ -1,15 +1,19 @@
 
-<form class="newsletter">
+<form action="//masterwp.us13.list-manage.com/subscribe/post?u=bf5d11ade9be022f552b86e69&amp;id=dd7ca5de89" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="newsletter" target="_blank" novalidate>
 
     <label>
         <span>Name:</span>
-        <input type="text" name="name" id="name" placeholder="Jo" required />
+        <input type="text" name="FNAME" id="name" placeholder="Jo" required />
     </label>
 
     <label>
         <span>Email:</span>
-        <input type="email" name="email" id="email" placeholder="jo@masterwp.co" required />
+        <input type="email" name="EMAIL" id="email" placeholder="jo@masterwp.co" required />
     </label>
 
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_bf5d11ade9be022f552b86e69_dd7ca5de89" tabindex="-1" value=""></div>
+
     <input type="submit" value="Start Lesson #1" />
+
 </form>
+
