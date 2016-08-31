9d8ae7770853bb40f73c9d397a3cab4ea2b24d7d
BinaryMoon <binarymoon@gmail.com>
2016-08-31 21:45:55 +0100

    move old homepage file into views folder

---
+++ views/_partials/head.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+
+        <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
+        <link rel="dns-prefetch" href="https://cdnjs.cloudflare.com" />
+
+        <title>Master WordPress</title>
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Catamaran:300,700|Playfair+Display:400,700" />
+        <link rel="stylesheet" href="assets/css/styles.css" />
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-55722-24', 'auto');
+          ga('send', 'pageview');
+
+        </script>
+
+    </head>
---
+++ views/home.php
@@ -0,0 +1,148 @@
+<?php include( '_partials/head.php' ); ?>
+
+    <body>
+
+        <header class="masthead">
+
+            <div class="container">
+
+                <div class="branding">
+                    <img src="assets/images/logo-mark.png" alt="MasterWP Logo" />
+                    <h1>Master<span>WP</span></h1>
+                </div>
+
+                <div class="banner">
+
+                    <h2>A free seven-day email course to become a <strong>WordPress Master</strong></h2>
+
+                    <p>Learn everything you need to master WordPress, not rely on developers, customize your WordPress theme, integrate marketing, secure your site, and more.</p>
+
+                    <?php include( '_partials/newsletter.php' ); ?>
+
+                </div>
+
+            </div>
+
+        </header>
+
+        <section class="intro">
+
+            <div class="content">
+
+                <p><strong>This free email course will equip you to do everything you want to do with WordPress, yourself</strong>.</p>
+
+                <p>Most WordPress training courses focus on the basics: how to add pages, how to install themes. If you want to do anything more complicated they say "hire a developer".</p>
+
+                <p>This is fine, but <strong>what about users who want to do everything a developer can do</strong>?</p>
+
+                <p>This is where we come in. The week of lessons we've prepared will provide you with the knowledge and skills to do everything you want with WordPress yourself, without the need for paid help. You'll learn how to:</p>
+
+                <ul>
+                    <li>Set up your WordPress site how you want it</li>
+                    <li>Manage sites for clients</li>
+                    <li>Master speed and security (these are super important skills)</li>
+                    <li>Increase the value of your WordPress skills</li>
+                    <li>+ a ton more.</li>
+                </ul>
+
+                <p>Ready to get started? Enter your email below to learn the skills you need to <em>Become a WordPress Master</em>.</p>
+
+                <?php include( '_partials/newsletter.php' ); ?>
+
+            </div>
+
+        </section>
+
+        <hr />
+
+        <section class="about-us">
+
+            <h2>About the Authors</h2>
+
+            <div class="people">
+
+                <div class="person">
+
+                    <img src="assets/images/profile-ben.jpg" alt="Ben Gillbanks - WordPress developer" />
+
+                    <p><strong>Hi, I’m Ben!</strong> I’m a WordPress theme developer. I built one of the most popular themes on WordPress.com, one of the first “magazine style” themes and co-founded leading WordPress theme shop <a href="https://prothemedesign.com/">Pro Theme Design</a>.</p>
+
+                    <p>I’m an experienced developer, serving for over a decade as Director of Web Development at one of the largest websites in the world. I’ll be leading the technical lessons to equip you with the skills you need to Master WordPress.</p>
+
+                </div>
+
+                <div class="person">
+
+                    <img src="assets/images/profile-alex.jpg" alt="Alex Denning - WordPress power-user" />
+
+                    <p><strong>Hey, I’m Alex!</strong> I’m a WordPress power-user. I founded one of the most popular WordPress tutorial blogs and have written for Smashing Magazine, WPShout, WPZOOM and more.</p>
+
+                    <p>I’ve created documentation and video tutorials for WordPress companies and understand what users need to know. I’ll be leading the knowhow-based lessons so you can handle everything you need with WordPress yourself.</p>
+
+                </div>
+
+            </div>
+
+        </section>
+
+        <section class="course-lessons">
+
+            <div class="content">
+
+                <p>We'll deliver you seven lessons over seven days, right into your inbox. You'll learn:</p>
+
+                <ul class="lessons">
+                    <li>
+                        <?php include( 'assets/svg/eye.svg' ); ?>
+                        <strong>Lesson 1:</strong> How to choose the perfect WordPress theme
+                    </li>
+                    <li>
+                        <?php include( 'assets/svg/message.svg' ); ?>
+                        <strong>Lesson 2:</strong> Get the most from theme support
+                    </li>
+                    <li>
+                        <?php include( 'assets/svg/code.svg' ); ?>
+                        <strong>Lesson 3:</strong> Theme customization basics (+ helpful plugins)
+                    </li>
+                    <li>
+                        <?php include( 'assets/svg/lightning.svg' ); ?>
+                        <strong>Lesson 4:</strong> Make your site load really quickly
+                    </li>
+                    <li>
+                        <?php include( 'assets/svg/photo.svg' ); ?>
+                        <strong>Lesson 5:</strong> Using CDNs for extra speed
+                    </li>
+                    <li>
+                        <?php include( 'assets/svg/mail.svg' ); ?>
+                        <strong>Lesson 6:</strong> Integrating email marketing with WordPress
+                    </li>
+                    <li>
+                        <?php include( 'assets/svg/lock.svg' ); ?>
+                        <strong>Lesson 7:</strong> Keeping your WordPress site secure
+                    </li>
+                </ul>
+
+            </div>
+
+        </section>
+
+        <footer>
+
+            <div class="content">
+
+                <p>Still unsure about signing up? Become a WordPress Master is 100% free, there’s literally nothing to lose and you can unsubscribe at any time. Sign up and start the course immediately.</p>
+
+                <?php include( '_partials/newsletter.php' ); ?>
+
+            </div>
+
+            <div class="copyright">
+
+                &copy; 2016 MasterWP - design by <a href="https://prothemedesign.com" rel="nofollow">Pro Theme Design</a>
+
+            </div>
+
+        </footer>
+
+    </body>
+</html>
