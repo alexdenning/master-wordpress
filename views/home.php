cb2cf70bac51ecac3cddfacec43125ed5912fd45
BinaryMoon <binarymoon@gmail.com>
2016-09-13 10:14:16 +0100

    move the authors section into a partial so it can be reused in other pages

--- views/home.php
+++ views/home.php
@@ -44,35 +44,7 @@
 
         <hr />
 
-        <section class="about-us">
-
-            <h2>About the Authors</h2>
-
-            <div class="people">
-
-                <div class="person">
-
-                    <img src="assets/images/profile-ben.jpg" alt="Ben Gillbanks - WordPress developer" />
-
-                    <p><strong>Hi, I’m Ben!</strong> I’m a WordPress theme developer. I built one of the most popular themes on WordPress.com, one of the first “magazine style” themes and co-founded leading WordPress theme shop <a href="https://prothemedesign.com/">Pro Theme Design</a>.</p>
-
-                    <p>I’m an experienced developer, serving for over a decade as Director of Web Development at one of the largest websites in the world. I’ll be leading the technical lessons to equip you with the skills you need to Master WordPress.</p>
-
-                </div>
-
-                <div class="person">
-
-                    <img src="assets/images/profile-alex.jpg" alt="Alex Denning - WordPress power-user" />
-
-                    <p><strong>Hey, I’m Alex!</strong> I’m a WordPress power-user. I founded one of the most popular WordPress tutorial blogs and have written for Smashing Magazine, WPShout, WPZOOM and more.</p>
-
-                    <p>I’ve created documentation and video tutorials for WordPress companies and understand what users need to know. I’ll be leading the knowhow-based lessons so you can handle everything you need with WordPress yourself.</p>
-
-                </div>
-
-            </div>
-
-        </section>
+        <?php include( '_partials/authors.php' ); ?>
 
         <section class="course-lessons">
 
@@ -116,7 +88,7 @@
         </section>
 
         <div class="hello-bar">
-            MasterWP starts 12th September. <span>Don't miss out: sign up for free course now.</span>
+            Don't miss out: sign up for the free course now.
         </div>
 
 <?php
