39ab49931a489d441001c66759720d26227a437d
BinaryMoon <binarymoon@gmail.com>
2016-09-13 13:59:06 +0100

    split helpscout beacon into a separate partial

--- views/preorder.php
+++ views/preorder.php
@@ -25,22 +25,7 @@
             Preorder before October 23rd and save 20%!
         </div>
 
-        <script>
-
-            !function(e,o,n){window.HSCW=o,window.HS=n,n.beacon=n.beacon||{};var t=n.beacon;t.userConfig={},t.readyQueue=[],t.config=function(e){this.userConfig=e},t.ready=function(e){this.readyQueue.push(e)},o.config={docs:{enabled:!1,baseUrl:""},contact:{enabled:!0,formId:"88da5d52-79b0-11e6-91aa-0a5fecc78a4d"}};var r=e.getElementsByTagName("script")[0],c=e.createElement("script");c.type="text/javascript",c.async=!0,c.src="https://djtflbt20bdde.cloudfront.net/",r.parentNode.insertBefore(c,r)}(document,window.HSCW||{},window.HS||{});
-
-        </script>
-
-        <script>
-
-            HS.beacon.config({
-                color: '#E74C3C',
-                icon: 'message',
-                attachment: false,
-                instructions:'Question about the course? Send us a message and we\'ll be happy to help.'
-            });
-
-        </script>
-
 <?php
+    include( '_partials/presales.php' );
+
     include( '_partials/footer.php' );
