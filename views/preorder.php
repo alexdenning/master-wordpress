4d701e27fb6f8661994eb8ea1a093e50eceac6c5
BinaryMoon <binarymoon@gmail.com>
2016-09-14 09:06:52 +0100

    update preorder page layout

--- views/preorder.php
+++ views/preorder.php
@@ -1,11 +1,13 @@
 <?php include( '_partials/head.php' ); ?>
 
+            <script src="https://gumroad.com/js/gumroad.js"></script>
+
                 <div class="banner">
 
                     <h2>Finally, a course that teaches you to <strong>do anything you want with WordPress</strong></h2>
 
                     <p>A course for WordPress users, bloggers, creatives and web professionals to <strong>stop relying on developers</strong> and <strong>Master WordPress</strong>. Do anything you want, yourself.</p>
-                    
+
                 </div>
 
             </div>
@@ -14,111 +16,157 @@
 
         <section class="intro">
 
-<div class="content">
-  <p><strong>MasterWP will
-  take your WordPress skills to the next level.</strong> You'll be elevated from user to power user – with the skills to produce customized, fast and secure WordPress sites. </p>
+            <div class="content">
 
-<p>Most WordPress training courses, tutorials and info from theme shops focus on the basics: how to add pages, how to install themes. If you want to do anything more complicated they say "hire a developer".</p>
+                <p><strong>MasterWP will take your WordPress skills to the next level.</strong> You'll be elevated from user to power user – with the skills to produce customized, fast and secure WordPress sites.</p>
 
-<p>This is fine, but what about users who want to do everything a developer can do?</p><p><strong>This course bridges the gap between users and developers</strong>.</p><p>BUTTON Start learning today
-</p>
-<hr/>
+                <p>Most WordPress training courses, tutorials and info from theme shops focus on the basics: how to add pages, how to install themes. If you want to do anything more complicated they say "hire a developer".</p>
 
-<p>Over 4 modules and 38 lessons with 20+ hours of video content, you'll learn:
-</p>
+                <p>This is fine, but what about users who want to do everything a developer can do?</p>
 
-<ul>
-	<li>Module 1: WordPress crash-course</li>
-	<li>Get up to speed on basics</li>
-	<li>Know way around the editor</li>
-	<li>Installing plugins and themes</li>
-	<li>Advanced editor screen usage (shortcodes, HTML view)</li>
-	<li>Setting a static front page</li>
-	<li>Using media (better stock photos; editing images).</li>
-	<li>Advanced widget usage (using Jetpack to only display on x pages).</li>
-	
-	<li>Module 2: Using and customising themes</li>
-	
-	<li>Getting to grips with premium themes</li>
-	<li>How to choose quality themes - Alex</li>
-	<li>Why you should avoid certain types of themes (eg kitchen sink themes = really hard to customize)</li>
-	<li>Recommended premium theme stores</li>
-	<li>[email course] How to use support (I wrote a Smashing Mag article on this). - Alex</li>
-	<li>Customising your premium theme</li>
-	<li>Intro: making the changes you want (without a developer).</li>
-	<li>[email course] Customization basics, helpful plugins to edit your theme</li>
-	<li>https://docs.google.com/document/d/1bzUwPeGTK-3J6SFGPoGlK1PQ0hbwW5juy_pBPLp1tkU/edit#</li>
-	<li>Why you should use plugins rather than editing the theme.</li>
-	<li>CSS basics (colours, fonts etc) - and using Inspect element</li>
-	<li>Make logo bigger, customize header and footer.</li>
-	<li>Advanced changes with child themes.</li>
-	<li>Theme Customization services (like codeable.io) for those who don’t want to learn themselves</li>
-	
-	<li>Module 3: Extending WordPress</li>
-	
-	<li>Making your site fast</li>
-	<li>Getting the most out of your host - maybe include the fact that you get what you pay for - ie cheap hosts will be slow since you’ll be sharing the server with many others.</li>
-	<li>[email course] Make your site load really quickly</li>
-	
-	<li>[email course] Photon and CDNs - Ben?</li>
-	<li>Cloudflare - getting the basics right</li>
-	<li>Cloudflare - https and http2</li>
-	
-	<li>Security and backups</li>
-	<li>[email course] Security introduction: Updating WordPress, themes and plugins - how to automate the process. Why it’s so important - Alex.</li>
-	<li>Hiding vulnerabilities (re-hashing this would be interesting - https://wpshout.com/stop-ethical-hacker-breaking-wordpress-site/) </li>
-	<li>Essential security plugins (show best plugins + how to use them + discounts).</li>
-	<li>Essential backup plugins</li>
-	<li>Monetisation techniques [come back to this after feedback]</li>
-	<li>Selling with eCommerce.</li>
-	<li>Advertising</li>
-	<li>Setting up ad areas</li>
-	<li>Excluding ads from regular visitors</li>
-	<li>Blocking adblock users?</li>
-	<li>Advanced WordPress</li>
-	<li>Using the customizer</li>
-	<li>Creating a custom widget</li>
-	<li>Creating a custom shortcode</li>
-	<li>Using a Starter Theme (underscores?)</li>
-	<li>Using GenerateWP to make simple improvements</li>
-	<li>REST API basics</li>
-	<li>What is the rest api</li>
-	<li>How is it useful for WordPress themes</li>
-	<li>What can we do with it now?</li>
-	<li>Useful resources for learning more about developing for WordPress</li>
-	<li>Working with clients</li>
-	<li>White labelling</li>
-	<li>Useful tools for bulk managing sites</li>
-	<li>How to stop clients breaking their site</li>
-	<li>Contracts (protecting yourself)</li>
-	<li>Frequently troubling problems</li>
-	<li>Wrong sized thumbnails with a new theme</li>
-	<li>A beginners guide to debugging WordPress problems</li>
-	<li>Keeping up to date with WordPress news (news resources)</li>
-	<li>SEO?</li>
-	<li>Other FAQs?</li>
-</ul>
-  <p>As well as the full course, you'll receive these wonderful bonuses:</p>
+                <p><strong>This course bridges the gap between users and developers</strong>.</p>
 
-<ul>
-  <li>35 teaching videos that accompany every single lesson in the course and every module introduction.</li>
-  <li>15 bonus videos, articles and cheatsheets answering the most common questions related to Mastering WordPress and providing at-a-glance summaries.</li>
-  <li>Members-only Slack channel for asking questions and sharing your learning journey.</li>
-  <li>Downloads: full course in PDF and HTML formats, HD videos, cheatsheets, code snippets and more...</li>
-  <li>Lifetime access: take the course at your own pace and get all future updates free.</li></ul>
+                <a class="button" href="https://gum.co/masterwp/preorder">Start Learning Today!</a>
 
-  <p>BIG BUTTON: Ready to start mastering WordPress? Preorder the course today and get an exclusive 20% discount <strike>$249</strike> $199. Buy now.</p>
-  
-  <hr/>
-  
-  <p>Then some social proof if we can find some, and FAQs below the about the authors section.</p>
-  </div>
+            </div>
+
+        </section>
+
+        <hr/>
+
+        <section class="course">
+
+            <h2>The Course</h2>
+
+            <div class="content">
+
+                <p>Over 4 modules and 38 lessons with 20+ hours of video content, you'll learn:</p>
+
+                <h3><strong>Module 1:</strong> WordPress crash-course</h3>
+
+                <ul>
+                    <li>Get up to speed on basics</li>
+                    <li>Know way around the editor</li>
+                    <li>Installing plugins and themes</li>
+                    <li>Advanced editor screen usage (shortcodes, HTML view)</li>
+                    <li>Setting a static front page</li>
+                    <li>Using media (better stock photos; editing images).</li>
+                    <li>Advanced widget usage (using Jetpack to only display on x pages).</li>
+                </ul>
+
+                <h3><strong>Module 2:</strong> Using and customising themes</h3>
+
+                <ul>
+                    <li>Getting to grips with premium themes</li>
+                    <li>How to use premium theme support</li>
+                    <li>Customising your premium theme</li>
+                    <li>Intro: making the changes you want (without a developer).</li>
+                    <li>Customization basics, helpful plugins to edit your theme</li>
+                    <li>Why you should use plugins rather than editing the theme.</li>
+                    <li>CSS basics (colours, fonts etc) - and using Inspect element</li>
+                    <li>Advanced changes with child themes.</li>
+                    <li>Theme Customization services (like codeable.io) for those who don’t want to learn themselves</li>
+                </ul>
+
+                <h3><strong>Module 3:</strong> Extending WordPress</h3>
+
+                <ul>
+                    <li>Making your site fast</li>
+                    <li>Getting the most out of your host</li>
+                    <li>Make your site load really quickly</li>
+                    <li>Jetpack Photon and CDNs</li>
+                    <li>Cloudflare - getting the basics right</li>
+                    <li>Cloudflare - https and http2</li>
+                    <li>Security introduction: Updating WordPress, themes and plugins - how to automate the process. Why it’s so important - Alex.</li>
+                    <li>Hiding vulnerabilities</li>
+                    <li>Essential security plugins.</li>
+                    <li>Essential backup plugins</li>
+                </ul>
+
+                <h3><strong>Module 4:</strong> Advanced WordPress</h3>
+
+                <ul>
+                    <li>Advanced WordPress</li>
+                    <li>Using the customizer</li>
+                    <li>Creating a custom widget</li>
+                    <li>Creating a custom shortcode</li>
+                    <li>Using a Starter Theme (underscores?)</li>
+                    <li>Using GenerateWP to make simple improvements</li>
+                    <li>REST API basics</li>
+                    <li>What is the REST API?</li>
+
+
+                    <li>Useful resources for learning more about developing for WordPress</li>
+                    <li>Working with clients</li>
+                    <li>White labelling</li>
+                    <li>Useful tools for bulk managing sites</li>
+                    <li>How to stop clients breaking their site</li>
+                    <li>Contracts (protecting yourself)</li>
+                </ul>
+
+                <h3><strong>Extra Lessons:</strong> Frequently troubling problems</h3>
+
+                <ul>
+                    <li>Wrong sized thumbnails with a new theme</li>
+                    <li>A beginners guide to debugging WordPress problems</li>
+                    <li>Keeping up to date with WordPress news (news resources)</li>
+                    <li>SEO?</li>
+                    <li>Other FAQs?</li>
+                </ul>
+
+                <hr />
+
+                <h2>Bonus Content</h2>
+
+                <p>As well as the full course, you'll receive these wonderful bonuses:</p>
+
+                <ul>
+                    <li>35 teaching videos that accompany every single lesson in the course and every module introduction.</li>
+                    <li>15 bonus videos, articles and cheatsheets answering the most common questions related to Mastering WordPress and providing at-a-glance summaries.</li>
+                    <li>Members-only Slack channel for asking questions and sharing your learning journey.</li>
+                    <li>Downloads: full course in PDF and HTML formats, HD videos, cheatsheets, code snippets and more...</li>
+                    <li>Lifetime access: take the course at your own pace and get all future updates free.</li>
+                </ul>
+
+                <div class="buy-now">
+                    <p>Ready to start mastering WordPress? Preorder the course today and get an exclusive 20% discount <strike>$249</strike> $199.</p>
+                    <a class="button" href="https://gum.co/masterwp/preorder">Buy now!</a>
+                </div>
+
+            </div>
 
         </section>
 
         <hr />
 
         <?php include( '_partials/authors.php' ); ?>
+
+        <section class="faq">
+
+            <div class="content">
+
+                <h2>FAQ</h2>
+
+                <dl>
+
+                    <dt>A question?</dt>
+                    <dd>An answer</dd>
+
+                    <dt>A question?</dt>
+                    <dd>An answer</dd>
+
+                    <dt>A question?</dt>
+                    <dd>An answer</dd>
+
+                    <dt>A question?</dt>
+                    <dd>An answer</dd>
+
+                </dl>
+
+            </div>
+
+        </section>
+
 
         <div class="hello-bar">
             Preorder before October 23rd and save 20%!
