6fdb91ece7bff38942313209a7f32d45e8672713
BinaryMoon <binarymoon@gmail.com>
2016-09-15 16:09:31 +0100

    update preorder page

--- views/preorder.php
+++ views/preorder.php
@@ -1,12 +1,21 @@
 <?php include( '_partials/head.php' ); ?>
 
-            <script src="https://gumroad.com/js/gumroad.js"></script>
+                <div class="sub-header">
+
+                    <h1>Become a WordPress Master</h1>
+                    <p class="description">The complete course for doing everything you want with WordPress</p>
+
+                </div>
+
+                <script src="https://gumroad.com/js/gumroad.js"></script>
 
                 <div class="banner">
 
                     <h2>Finally, a course that teaches you to <strong>do anything you want with WordPress</strong></h2>
 
                     <p>A course for WordPress users, bloggers, creatives and web professionals to <strong>stop relying on developers</strong> and <strong>Master WordPress</strong>. Do anything you want, yourself.</p>
+
+                    <a class="button" href="https://gum.co/masterwp/preorder">Start Learning Today!</a>
 
                 </div>
 
@@ -38,102 +47,185 @@
 
             <h2>The Course</h2>
 
+            <p class="description">Over <strong>4 modules</strong> and <strong>38 lessons</strong> with <strong>20+ hours of video content</strong>, you'll learn:</p>
+
+            <div class="modules">
+
+                <div class="module">
+
+                    <?php include( 'assets/svg/settings.svg' ); ?>
+
+                    <h3><strong>Module 1:</strong> WordPress crash-course</h3>
+
+                    <ul>
+                        <li>Get up to speed on basics</li>
+                        <li>Know your way around the editor</li>
+                        <li>Installing plugins and themes</li>
+                        <li>Advanced editor screen usage (shortcodes, HTML view)</li>
+                        <li>Setting a static front page</li>
+                        <li>Using media (better stock photos; editing images).</li>
+                        <li>Advanced widget usage (using Jetpack to only display on certain pages).</li>
+                    </ul>
+
+                </div>
+
+                <div class="module">
+
+                    <?php include( 'assets/svg/options.svg' ); ?>
+
+                    <h3><strong>Module 2:</strong> Using and customising themes</h3>
+
+                    <ul>
+                        <li>Getting to grips with premium themes</li>
+                        <li>How to use premium theme support <span class="read-now">Read Now</span></li>
+                        <li>Customising your premium theme</li>
+                        <li>Intro: making the changes you want (without a developer).</li>
+                        <li>Customization basics, helpful plugins to edit your theme. <span class="read-now">Read Now</span></li>
+                        <li>Why you should use plugins rather than editing the theme.</li>
+                        <li>CSS basics (colours, fonts etc) - and using Inspect element</li>
+                        <li>Advanced changes with child themes.</li>
+                        <li>Theme Customization services (like codeable.io) for those who don’t want to learn themselves</li>
+                    </ul>
+
+                </div>
+
+                <div class="module">
+
+                    <?php include( 'assets/svg/work.svg' ); ?>
+
+                    <h3><strong>Module 3:</strong> Extending WordPress</h3>
+
+                    <ul>
+                        <li>Making your site fast</li>
+                        <li>Getting the most out of your host</li>
+                        <li>Make your site load really quickly</li>
+                        <li>Jetpack Photon and CDNs. <span class="read-now">Read Now</span></li>
+                        <li>Cloudflare - getting the basics right</li>
+                        <li>Cloudflare - https and http2</li>
+                        <li>Security introduction: Updating WordPress, themes and plugins - how to automate the process. Why it’s so important. <span class="read-now">Read Now</span></li>
+                        <li>Hiding vulnerabilities.</li>
+                        <li>Essential security plugins.</li>
+                        <li>Essential backup plugins</li>
+                    </ul>
+
+                </div>
+
+                <div class="module">
+
+                    <?php include( 'assets/svg/star.svg' ); ?>
+
+                    <h3><strong>Module 4:</strong> Advanced WordPress</h3>
+
+                    <ul>
+                        <li>Advanced WordPress</li>
+                        <li>Using the customizer</li>
+                        <li>Creating a custom widget</li>
+                        <li>Creating a custom shortcode</li>
+                        <li>Using a Starter Theme (underscores?)</li>
+                        <li>Using GenerateWP to make simple improvements</li>
+                        <li>REST API basics</li>
+                        <li>What is the REST API?</li>
+
+
+                        <li>Useful resources for learning more about developing for WordPress</li>
+                        <li>Working with clients</li>
+                        <li>White labelling</li>
+                        <li>Useful tools for bulk managing sites</li>
+                        <li>How to stop clients breaking their site</li>
+                        <li>Contracts (protecting yourself)</li>
+                    </ul>
+
+                </div>
+
+                <div class="module">
+
+                    <?php include( 'assets/svg/info.svg' ); ?>
+
+                    <h3><strong>Extra Lessons:</strong> Frequently troubling problems</h3>
+
+                    <ul>
+                        <li>Wrong sized thumbnails with a new theme</li>
+                        <li>A beginners guide to debugging WordPress problems</li>
+                        <li>Keeping up to date with WordPress news (news resources)</li>
+                        <li>SEO?</li>
+                        <li>Other FAQs?</li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+            <hr />
+
             <div class="content">
-
-                <p>Over 4 modules and 38 lessons with 20+ hours of video content, you'll learn:</p>
-
-                <h3><strong>Module 1:</strong> WordPress crash-course</h3>
-
-                <ul>
-                    <li>Get up to speed on basics</li>
-                    <li>Know way around the editor</li>
-                    <li>Installing plugins and themes</li>
-                    <li>Advanced editor screen usage (shortcodes, HTML view)</li>
-                    <li>Setting a static front page</li>
-                    <li>Using media (better stock photos; editing images).</li>
-                    <li>Advanced widget usage (using Jetpack to only display on x pages).</li>
-                </ul>
-
-                <h3><strong>Module 2:</strong> Using and customising themes</h3>
-
-                <ul>
-                    <li>Getting to grips with premium themes</li>
-                    <li>How to use premium theme support</li>
-                    <li>Customising your premium theme</li>
-                    <li>Intro: making the changes you want (without a developer).</li>
-                    <li>Customization basics, helpful plugins to edit your theme</li>
-                    <li>Why you should use plugins rather than editing the theme.</li>
-                    <li>CSS basics (colours, fonts etc) - and using Inspect element</li>
-                    <li>Advanced changes with child themes.</li>
-                    <li>Theme Customization services (like codeable.io) for those who don’t want to learn themselves</li>
-                </ul>
-
-                <h3><strong>Module 3:</strong> Extending WordPress</h3>
-
-                <ul>
-                    <li>Making your site fast</li>
-                    <li>Getting the most out of your host</li>
-                    <li>Make your site load really quickly</li>
-                    <li>Jetpack Photon and CDNs</li>
-                    <li>Cloudflare - getting the basics right</li>
-                    <li>Cloudflare - https and http2</li>
-                    <li>Security introduction: Updating WordPress, themes and plugins - how to automate the process. Why it’s so important - Alex.</li>
-                    <li>Hiding vulnerabilities</li>
-                    <li>Essential security plugins.</li>
-                    <li>Essential backup plugins</li>
-                </ul>
-
-                <h3><strong>Module 4:</strong> Advanced WordPress</h3>
-
-                <ul>
-                    <li>Advanced WordPress</li>
-                    <li>Using the customizer</li>
-                    <li>Creating a custom widget</li>
-                    <li>Creating a custom shortcode</li>
-                    <li>Using a Starter Theme (underscores?)</li>
-                    <li>Using GenerateWP to make simple improvements</li>
-                    <li>REST API basics</li>
-                    <li>What is the REST API?</li>
-
-
-                    <li>Useful resources for learning more about developing for WordPress</li>
-                    <li>Working with clients</li>
-                    <li>White labelling</li>
-                    <li>Useful tools for bulk managing sites</li>
-                    <li>How to stop clients breaking their site</li>
-                    <li>Contracts (protecting yourself)</li>
-                </ul>
-
-                <h3><strong>Extra Lessons:</strong> Frequently troubling problems</h3>
-
-                <ul>
-                    <li>Wrong sized thumbnails with a new theme</li>
-                    <li>A beginners guide to debugging WordPress problems</li>
-                    <li>Keeping up to date with WordPress news (news resources)</li>
-                    <li>SEO?</li>
-                    <li>Other FAQs?</li>
-                </ul>
-
-                <hr />
 
                 <h2>Bonus Content</h2>
 
                 <p>As well as the full course, you'll receive these wonderful bonuses:</p>
 
                 <ul>
-                    <li>35 teaching videos that accompany every single lesson in the course and every module introduction.</li>
-                    <li>15 bonus videos, articles and cheatsheets answering the most common questions related to Mastering WordPress and providing at-a-glance summaries.</li>
-                    <li>Members-only Slack channel for asking questions and sharing your learning journey.</li>
+                    <li><strong>35 teaching videos</strong> that accompany every single lesson in the course and every module introduction.</li>
+                    <li><strong>15 bonus videos, articles and cheatsheets</strong> answering the most common questions related to Mastering WordPress and providing at-a-glance summaries.</li>
+                    <li><strong>Members-only Slack channel</strong> for asking questions and sharing your learning journey.</li>
                     <li>Downloads: full course in PDF and HTML formats, HD videos, cheatsheets, code snippets and more...</li>
-                    <li>Lifetime access: take the course at your own pace and get all future updates free.</li>
+                    <li><strong>Lifetime access:</strong> take the course at your own pace and get all future updates free.</li>
                 </ul>
 
-                <div class="buy-now">
-                    <p>Ready to start mastering WordPress? Preorder the course today and get an exclusive 20% discount <strike>$249</strike> $199.</p>
+            </div>
+
+        </section>
+
+        <section class="pricing">
+
+            <h2>Start Learning</h2>
+
+            <p class="description">Pre-order TODAY to <strong>save 20%</strong><br />Offer ends October 23rd!</p>
+            <div class="packages">
+
+                <div class="package">
+                    <h3>Lite</h3>
+                    <ul>
+                        <li>feature 1</li>
+                        <li>feature 2</li>
+                        <li>feature 3</li>
+                        <li>feature 4</li>
+                        <li>feature 5</li>
+                        <li>feature 6</li>
+                    </ul>
+                    <h4><strike>$90</strike> $70</h4>
+                    <a class="button" href="https://gum.co/masterwp/preorder">Buy now!</a>
+                </div>
+
+                <div class="package best">
+                    <h3>Master</h3>
+                    <ul>
+                        <li>feature 1</li>
+                        <li>feature 2</li>
+                        <li>feature 3</li>
+                        <li>feature 4</li>
+                        <li>feature 5</li>
+                        <li>feature 6</li>
+                    </ul>
+                    <h4><strike>$250</strike> $200</h4>
+                    <a class="button" href="https://gum.co/masterwp/preorder">Buy now!</a>
+                </div>
+
+                <div class="package">
+                    <h3>All In</h3>
+                    <ul>
+                        <li>feature 1</li>
+                        <li>feature 2</li>
+                        <li>feature 3</li>
+                        <li>feature 4</li>
+                        <li>feature 5</li>
+                        <li>feature 6</li>
+                    </ul>
+                    <h4><strike>$750</strike> $500</h4>
                     <a class="button" href="https://gum.co/masterwp/preorder">Buy now!</a>
                 </div>
 
             </div>
+
 
         </section>
 
