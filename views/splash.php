91711aae9ba265b999895510b80d17bec6c17438
BinaryMoon <binarymoon@gmail.com>
2016-09-22 22:36:38 +0100

    update splash screen

---
+++ views/splash.php
@@ -0,0 +1,24 @@
+<?php include( '_partials/head.php' ); ?>
+
+                <div class="banner">
+
+                    <h2>Finally, a course that teaches you to <strong>do anything you want with WordPress</strong></h2>
+
+                    <p>A course for WordPress users, bloggers, creatives and web professionals to <strong>stop relying on developers</strong> and <strong>Master WordPress</strong>. Do anything you want, yourself.</p>
+
+                    <h3>Find out when we launch!</h3>
+
+                    <?php include( '_partials/newsletter.php' ); ?>
+
+                </div>
+
+            </div>
+
+        </header>
+
+        <?php include( '_partials/authors.php' ); ?>
+
+<?php
+    include( '_partials/presales.php' );
+
+    include( '_partials/footer.php' );
