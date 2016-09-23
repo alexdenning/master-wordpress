6f9eed2d2644c2d409492e2a433dc8d53a4b43bf
alexdenning <alexdenning@gmail.com>
2016-09-23 09:15:50 +0100

    Added some 100% legit terms and conditions
    I'll get my mum to have a look over (she's a lawyer).

--- views/terms.php
+++ views/terms.php
@@ -14,7 +14,23 @@
 
             <div class="content">
 
-                <p>Terms go in here.</p>
+                <p>This is a non-exclusive, non-transferable, paid-in-full licence to access, read, and use “Become a WordPress Master”.</p>
+                
+                <p>All rights not specified in this agreement are expressly reserved by Alex Denning and Ben Gillbanks. “We” and “our” is defined in this licence as Alex Denning and Ben Gillbanks.</p>
+                
+                <p>All disputes related to this licensing agreement shall be resolved in a court located in the United Kingdom. By purchasing a license, you consent and submit to this jurisdiction. This agreement shall be governed by United Kingdom law.</p>
+                
+                <p>If a court of competent jurisdiction finds any part of this agreement to be invalid, the remaining provisions shall continue in full force and effect.</p>
+                
+                <p>We intend to keep <em>Become a WordPress Master</em> available for a long time. However, “lifetime access” defined as the lifetime of the product, which can cease at our discretion. In the event our original distribution methods are no longer available we will provide alternative means of access or request customers email hello@masterwp.co to receive access.</p>
+                
+                <p><em>Become a WordPress Master</em> is offered “as-is” with no guarantee of updates. Furthermore, we will not be held liable for anything ceasing to work as a result of our course and are not necessarily offering any technical support.</p>
+                
+                <p>All purchases must be made by card using Gumroad’s payment processing service. By making this purchase, you also agree to <a href="https://gumroad.com/">their user agreement</a>. Alex and Ben do not receive or hold any payment data.</p>
+                
+                <p>We offer a 30 day money-back guarantee. Contact hello@masterwp.co within this timeframe and we will be happy to initiate a refund on your purchase. Refunds are not offered after this period. </p>
+                
+                <p>Alex and Ben reserve the right to modify this agreement at any time and with no prior notice.</p>
 
             </div>
 
