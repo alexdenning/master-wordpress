e74a11671b0323cfffe734395d1860204f23b13b
alexdenning <alexdenning@gmail.com>
2016-09-23 12:10:44 +0100

    small change to copy

--- views/splash.php
+++ views/splash.php
@@ -6,7 +6,7 @@
 
                     <p>A course for WordPress users, bloggers, creatives and web professionals to <strong>stop relying on developers</strong> and <strong>Master WordPress</strong>. Do anything you want, yourself.</p>
 
-                    <h3>Find out when we launch!</h3>
+                    <h3>Get an exclusive discount when we launch!</h3>
 
                     <?php include( '_partials/newsletter.php' ); ?>
 
