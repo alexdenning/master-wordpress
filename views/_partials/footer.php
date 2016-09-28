e1a1df888ffc6814a4dafd4ebf042df971dbf552
BinaryMoon <binarymoon@gmail.com>
2016-09-28 14:50:55 +0100

    change copyright date to be dynamic, and add current date as comment in footer

--- views/_partials/footer.php
+++ views/_partials/footer.php
@@ -18,9 +18,11 @@
 
             <div class="copyright">
 
-                &copy; 2016 MasterWP - design by <a href="https://prothemedesign.com" rel="nofollow">Pro Theme Design</a> - <a href="<?php echo path( 'terms-and-conditions' ); ?>">Terms</a>
+                &copy; <?php echo date( 'Y' ); ?> MasterWP - design by <a href="https://prothemedesign.com" rel="nofollow">Pro Theme Design</a> - <a href="<?php echo path( 'terms-and-conditions' ); ?>">Terms</a>
 
             </div>
+
+            <!-- <?php echo gmdate( 'jS F o Hi' ); ?> -->
 
         </footer>
 
