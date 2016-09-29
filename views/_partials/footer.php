0e2c3e7ab9bf441615b6413016d47d5e564ec370
BinaryMoon <binarymoon@gmail.com>
2016-09-29 11:19:11 +0100

    remove newsletter signup from homepage

--- views/_partials/footer.php
+++ views/_partials/footer.php
@@ -1,21 +1,5 @@
         <footer>
 
-<?php
-    if ( page_is( '/' ) ) {
-?>
-
-            <div class="content">
-
-                <p>Still unsure about signing up? Become a WordPress Master is 100% free, there’s literally nothing to lose and you can unsubscribe at any time. Sign up and start the course immediately.</p>
-
-                <?php site_include_view( '_partials/newsletter.php' ); ?>
-
-            </div>
-
-<?php
-    }
-?>
-
             <div class="copyright">
 
                 &copy; <?php echo date( 'Y' ); ?> MasterWP - design by <a href="https://prothemedesign.com" rel="nofollow">Pro Theme Design</a> - <a href="<?php echo path( 'terms-and-conditions' ); ?>">Terms</a>
