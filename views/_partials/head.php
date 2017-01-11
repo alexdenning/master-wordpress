e70c5b9361fb406fd870fc68d115b3ba12d74184
BinaryMoon <binarymoon@gmail.com>
2017-01-11 15:27:59 +0000

    update head

--- views/_partials/head.php
+++ views/_partials/head.php
@@ -1,67 +1,67 @@
 <!doctype html>
 <html class="no-js" lang="en">
-    <head>
+	<head>
 
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="profile" href="http://gmpg.org/xfn/11">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
 
-        <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
-        <link rel="dns-prefetch" href="https://cdnjs.cloudflare.com" />
+		<link rel="dns-prefetch" href="https://fonts.googleapis.com" />
+		<link rel="dns-prefetch" href="https://cdnjs.cloudflare.com" />
 
-        <title>Master WordPress - Level Up Your WordPress Knowledge</title>
-        <meta name="description" content="Take your WordPress skills to the next level with this free 7 day course.">
+		<title>Master WordPress - Level Up Your WordPress Knowledge</title>
+		<meta name="description" content="Take your WordPress skills to the next level with this free 7 day course.">
 
-        <!-- Site Stuff -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Catamaran:300,700|Playfair+Display:400,700" />
-        <link rel="stylesheet" href="<?php echo css_path( 'styles.css' ); ?>" />
+		<!-- Site Stuff -->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Catamaran:300,700|Playfair+Display:400,700" />
+		<link rel="stylesheet" href="<?php echo css_path( 'styles.css' ); ?>" />
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        <script src="https://gumroad.com/js/gumroad.js"></script>
-        <script src="<?php echo scripts_path( 'main.js' ); ?>"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script src="https://gumroad.com/js/gumroad.js"></script>
+		<script src="<?php echo scripts_path( 'main.js' ); ?>"></script>
 
-        <!-- Website Analytics -->
-        <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		<!-- Website Analytics -->
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-          ga('create', 'UA-55722-24', 'auto');
-          ga('send', 'pageview');
-        </script>
+		  ga('create', 'UA-55722-24', 'auto');
+		  ga('send', 'pageview');
+		</script>
 
-        <!-- Favicons -->
-        <link rel="icon" href="<?php echo image_path( 'favicon-32.png' ); ?>" sizes="32x32" />
-        <link rel="icon" href="<?php echo image_path( 'favicon-192.png' ); ?>" sizes="192x192" />
-        <link rel="apple-touch-icon-precomposed" href="<?php echo image_path( 'favicon-180.png' ); ?>" />
+		<!-- Favicons -->
+		<link rel="icon" href="<?php echo image_path( 'favicon-32.png' ); ?>" sizes="32x32" />
+		<link rel="icon" href="<?php echo image_path( 'favicon-192.png' ); ?>" sizes="192x192" />
+		<link rel="apple-touch-icon-precomposed" href="<?php echo image_path( 'favicon-180.png' ); ?>" />
 
-        <!-- Social Media Meta Tags -->
-        <meta itemprop="name" content="Master WordPress - Level Up Your WordPress Knowledge.">
-        <meta itemprop="description" content="Take your WordPress skills to the next level with this free 7 day course.">
+		<!-- Social Media Meta Tags -->
+		<meta itemprop="name" content="Master WordPress - Level Up Your WordPress Knowledge.">
+		<meta itemprop="description" content="Take your WordPress skills to the next level with this free 7 day course.">
 
-        <meta name="twitter:card" content="summary_large_image">
-        <meta name="twitter:site" content="@prothemedesign">
-        <meta name="twitter:title" content="Master WordPress - Level Up Your WordPress Knowledge.">
-        <meta name="twitter:description" content="Take your WordPress skills to the next level with this free 7 day course.">
-        <meta name="twitter:url" content="https://masterwp.co/">
-        <meta name="twitter:image" content="https://masterwp.co<?php echo image_path( 'banner.png' ); ?>" />
+		<meta name="twitter:card" content="summary_large_image">
+		<meta name="twitter:site" content="@prothemedesign">
+		<meta name="twitter:title" content="Master WordPress - Level Up Your WordPress Knowledge.">
+		<meta name="twitter:description" content="Take your WordPress skills to the next level with this free 7 day course.">
+		<meta name="twitter:url" content="https://masterwp.co/">
+		<meta name="twitter:image" content="https://masterwp.co<?php echo image_path( 'banner.png' ); ?>" />
 
-        <meta property="og:title" content="Master WordPress - Level Up Your WordPress Knowledge.">
-        <meta property="og:url" content="https://masterwp.co/">
-        <meta property="og:description" content="Take your WordPress skills to the next level with this free 7 day course.">
-        <meta property="og:site_name" content="Master WordPress - Level Up Your WordPress Knowledge.">
-        <meta property="og:image" content="https://masterwp.co<?php echo image_path( 'banner.png' ); ?>"/>
+		<meta property="og:title" content="Master WordPress - Level Up Your WordPress Knowledge.">
+		<meta property="og:url" content="https://masterwp.co/">
+		<meta property="og:description" content="Take your WordPress skills to the next level with this free 7 day course.">
+		<meta property="og:site_name" content="Master WordPress - Level Up Your WordPress Knowledge.">
+		<meta property="og:image" content="https://masterwp.co<?php echo image_path( 'banner.png' ); ?>"/>
 
-    </head>
+	</head>
 
-    <body>
+	<body>
 
-        <header class="masthead">
+		<header class="masthead">
 
-            <div class="container">
+			<div class="container">
 
-                <div class="branding">
-                    <img src="<?php echo image_path( 'logo-mark.png' ); ?>" alt="MasterWP Logo" />
-                    <h1>Master<span>WP</span></h1>
-                </div>
+				<div class="branding">
+					<img src="<?php echo image_path( 'logo-mark.png' ); ?>" alt="MasterWP Logo" />
+					<h1>Master<span>WP</span> Weekly</h1>
+				</div>
