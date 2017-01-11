b958ccb6c7dc6de05f78c1d1e7c01d29af0997e2
BinaryMoon <binarymoon@gmail.com>
2017-01-11 15:27:16 +0000

    update routes

--- library/routes.php
+++ library/routes.php
@@ -3,21 +3,21 @@
 // Home
 Flight::route( '/', function() {
 
-    $template = 'splash.php';
+	$template = 'weekly.php';
 
-    $date_now = (int) gmdate( 'U' );
+	// $date_now = (int) gmdate( 'U' );
+	//
+	// // +3 hours for server time offset.
+	// $date_start = strtotime( '28th September 2016 6pm GMT', $date_now ); // 3pm GMT
+	// $date_end = strtotime( '1st October 2016 2am GMT', $date_now ); // 10pm GMT
+	//
+	// if ( $date_now > $date_start && $date_now < $date_end ) {
+	//     $template = 'preorder.php';
+	// }
 
-    // +3 hours for server time offset.
-    $date_start = strtotime( '28th September 2016 6pm GMT', $date_now ); // 3pm GMT
-    $date_end = strtotime( '1st October 2016 2am GMT', $date_now ); // 10pm GMT
-
-    if ( $date_now > $date_start && $date_now < $date_end ) {
-        $template = 'preorder.php';
-    }
-
-    Flight::render(
-        $template
-    );
+	Flight::render(
+		$template
+	);
 
 } );
 
@@ -25,9 +25,9 @@ Flight::route( '/', function() {
 // Nearly Done
 Flight::route( '/nearly-done/', function() {
 
-    Flight::render(
-        'nearly-done.php'
-    );
+	Flight::render(
+		'nearly-done.php'
+	);
 
 } );
 
@@ -35,9 +35,9 @@ Flight::route( '/nearly-done/', function() {
 // Thank you
 Flight::route( '/thank-you/', function() {
 
-    Flight::render(
-        'thank-you.php'
-    );
+	Flight::render(
+		'thank-you.php'
+	);
 
 } );
 
@@ -45,18 +45,28 @@ Flight::route( '/thank-you/', function() {
 // Preorder
 Flight::route( '/preorder/', function() {
 
-    Flight::render(
-        'preorder.php'
-    );
+	Flight::render(
+		'preorder.php'
+	);
+
+} );
+
+
+// Weekly
+Flight::route( '/preorder/', function() {
+
+	Flight::render(
+		'weekly.php'
+	);
 
 } );
 
 // Terms and Conditions.
 Flight::route( '/terms-and-conditions/', function() {
 
-    Flight::render(
-        'terms.php'
-    );
+	Flight::render(
+		'terms.php'
+	);
 
 } );
 
@@ -64,10 +74,8 @@ Flight::route( '/terms-and-conditions/', function() {
 // Splash screen.
 Flight::route( '/splash/', function() {
 
-    Flight::render(
-        'splash.php'
-    );
+	Flight::render(
+		'splash.php'
+	);
 
 } );
-
-
