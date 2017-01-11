f0812cceb4ec4b6107ddf304b57adcf170d8914a
BinaryMoon <binarymoon@gmail.com>
2017-01-11 15:27:31 +0000

    add previous year to footer copyright

--- views/_partials/footer.php
+++ views/_partials/footer.php
@@ -1,14 +1,14 @@
-        <footer>
+		<footer>
 
-            <div class="copyright">
+			<div class="copyright">
 
-                &copy; <?php echo date( 'Y' ); ?> MasterWP - design by <a href="https://prothemedesign.com" rel="nofollow">Pro Theme Design</a> - <a href="<?php echo path( 'terms-and-conditions' ); ?>">Terms</a>
+				&copy; 2016 - <?php echo date( 'Y' ); ?> MasterWP - design by <a href="https://prothemedesign.com" rel="nofollow">Pro Theme Design</a> - <a href="<?php echo path( 'terms-and-conditions' ); ?>">Terms</a>
 
-            </div>
+			</div>
 
-            <!-- <?php echo gmdate( 'jS F o Hi' ); ?> -->
+			<!-- <?php echo gmdate( 'jS F o Hi' ); ?> -->
 
-        </footer>
+		</footer>
 
-    </body>
+	</body>
 </html>
