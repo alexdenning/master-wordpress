6d8db58e591cf21df6c4ffad110af6da66739025
BinaryMoon <binarymoon@gmail.com>
2017-01-11 15:28:16 +0000

    add weekly view

---
+++ views/weekly.php
@@ -0,0 +1,23 @@
+<?php include( '_partials/head.php' ); ?>
+
+				<div class="banner">
+
+					<h2>A newsletter for WordPress Masters</h2>
+
+					<p>MasterWP Weekly is a brief curated email newsletter sent every Wednesday. It contains a collection links that will be interesting to anyone involved in WordPress. Whether it's a WordPress article, or a useful tool - there will be something to improve your working life.</p>
+
+					<h3>Sign up now to join <em>397</em> other subscribers!</h3>
+					<?php include( '_partials/email-signup.php' ); ?>
+
+				</div>
+
+			</div>
+
+		</header>
+
+		<?php include( '_partials/authors.php' ); ?>
+
+<?php
+	include( '_partials/presales.php' );
+
+	include( '_partials/footer.php' );
