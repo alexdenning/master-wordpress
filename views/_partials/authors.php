af99fcb4871a4edbe2b94830db062e98498adb0a
alexdenning <alexdenning@gmail.com>
2017-01-24 15:00:23 +0000

    Update authors.php
    updated copy to remove "I'll teach you this"

--- views/_partials/authors.php
+++ views/_partials/authors.php
@@ -10,7 +10,7 @@
 
                     <p><strong>Hi, I’m Ben!</strong> I’m a WordPress theme developer. I built one of the most popular themes on WordPress.com, one of the first “magazine style” themes and co-founded leading WordPress theme shop <a href="https://prothemedesign.com/">Pro Theme Design</a>.</p>
 
-                    <p>I’m an experienced developer, serving for over a decade as Director of Web Development at one of the largest websites in the world. I’ll be leading the technical lessons to equip you with the skills you need to Master WordPress.</p>
+                    <p>I’m an experienced developer, serving for over a decade as Director of Web Development at one of the largest websites in the world.</p>
 
                 </div>
 
@@ -20,7 +20,7 @@
 
                     <p><strong>Hey, I’m Alex!</strong> I’m a WordPress power-user. I founded one of the most popular WordPress tutorial blogs and have written for Smashing Magazine, WPShout, WPZOOM and more.</p>
 
-                    <p>I’ve created documentation and video tutorials for WordPress companies and understand what users need to know. I’ll be leading the knowhow-based lessons so you can handle everything you need with WordPress yourself.</p>
+                    <p>I’ve created documentation and video tutorials for WordPress companies and understand what users need to know.</p>
 
                 </div>
 
