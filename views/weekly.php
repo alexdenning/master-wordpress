ec548cc2bba15629fc86db966f488c7321f1b1db
BinaryMoon <binarymoon@gmail.com>
2017-03-08 08:26:52 +0000

    add mailchimp campaign archives

--- views/weekly.php
+++ views/weekly.php
@@ -6,7 +6,7 @@
 
 					<p>MasterWP is a quality email newsletter sent every Wednesday, for WordPress professionals. Each week get a collection of apps, tools and links that will make life better and provoke thought.</p>
 					<p>No affiliates, no filler, no noise, just the good stuff.</p>
-					
+
 					<p>You're in good company: sign up now to join <strong>397</strong> other subscribers. Plus, it's free, and you can unsubscribe any time :)</p>
 					<?php include( '_partials/email-signup.php' ); ?>
 
@@ -15,6 +15,20 @@
 			</div>
 
 		</header>
+
+		<section class="intro">
+
+			<div class="content">
+
+				<h2>Past Issues</h2>
+
+				<script language="javascript" src="//masterwp.us13.list-manage.com/generate-js/?u=bf5d11ade9be022f552b86e69&fid=6313&show=10" type="text/javascript"></script>
+
+				<hr />
+
+			</div>
+
+		</section>
 
 		<?php include( '_partials/authors.php' ); ?>
 
